055f5630b2b5f6c569163653e62d70c7ad4dcc44
Luke Kosinski <46657629+lkosin@users.noreply.github.com>
2019-07-25 19:09:40 -0700

    Updated colors.

--- Scripts/figure4.R
+++ Scripts/figure4.R
@@ -24,7 +24,9 @@ hydrophilicity.lm <- lm(
 )
 summary(hydrophilicity.lm)
 
-png(filename = "Scripts/Figures/DisProp_L0_ribo_7-8-19.png", height = 400, width = 400)
+cbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+png(filename = "Scripts/Figures/DisProp_L0_ribo_7-25-19.png", height = 400, width = 400)
 ggplot(data = ribohits.data[ribohits.data$Fragile == 0,],
        aes(x = log(Length.0),
            y = Disorder.prop.0,
@@ -39,11 +41,11 @@ ggplot(data = ribohits.data[ribohits.data$Fragile == 0,],
   #                   labels = c(0.04, 0.16, 0.36),
   #                   limits = c(0.125, 0.725)) +
   theme_bw(base_size = 28) +
-  scale_color_manual(limits = c("Yes", "No"), values = c("#F8766D", "#00BFC4")) +
+  scale_color_manual(limits = c("Yes", "No"), values = cbPalette[c(2,4)]) +
   theme(legend.position = "none")
 dev.off()
 
-png(filename = "Scripts/Figures/Hydrophilic_L0_ribo_7-8-19.png", width = 400, height = 400)
+png(filename = "Scripts/Figures/Hydrophilic_L0_ribo_7-25-19.png", width = 400, height = 400)
 ggplot(data = ribohits.data[ribohits.data$Fragile == 0,],
        aes(x = log(Length.0),
            y = RSA.hydrophilicity.0,
@@ -58,7 +60,7 @@ ggplot(data = ribohits.data[ribohits.data$Fragile == 0,],
   #                   labels = c(0.04, 0.16, 0.36),
   #                   limits = c(0.125, 0.725)) +
   theme_bw(base_size = 28) +
-  scale_color_manual(limits = c("Yes", "No"), values = c("#F8766D", "#00BFC4")) +
+  scale_color_manual(limits = c("Yes", "No"), values = cbPalette[c(2,4)]) +
   theme(legend.position = c(0.7775, 0.8325),
         legend.background = element_rect(fill = "white", size = 1, linetype = "solid", color = "black"))
 dev.off()
